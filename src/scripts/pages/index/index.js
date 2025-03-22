5dbda50863a8d097ea6c75553c041923e4bb6a38
João Vitor Aleixo <76683458+joaoaleixovitor@users.noreply.github.com>
2025-03-22 12:53:35 -0300

    start mediaquery

---
+++ src/scripts/pages/index/index.js
@@ -0,0 +1,6 @@
+const buttonHamburguerMenu = document.querySelector("#buttonHamburguerMenu")
+const menu = document.querySelector("#menu-hamburguer")
+
+buttonHamburguerMenu.addEventListener("click", (el)=>{
+    menu.style.display = none
+})
