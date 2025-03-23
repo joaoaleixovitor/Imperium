bce31ddf337445e8cf391d3b97a21843127d84c2
João Vitor Aleixo <76683458+joaoaleixovitor@users.noreply.github.com>
2025-03-23 15:41:43 -0300

    update media query

--- src/scripts/pages/index/index.js
+++ src/scripts/pages/index/index.js
@@ -1,6 +1,10 @@
-const buttonHamburguerMenu = document.querySelector("#buttonHamburguerMenu")
-const menu = document.querySelector("#menu-hamburguer")
+const buttonHamburguerMenu = document.querySelector("#buttonHamburguerMenu");
+const header = document.querySelector("#header");
+const menu = document.querySelector("#menu-hamburguer");
+const head = document.querySelector("#premium_make_collection")
 
-buttonHamburguerMenu.addEventListener("click", (el)=>{
-    menu.style.display = none
+buttonHamburguerMenu.addEventListener("click", ()=>{
+    menu.classList.toggle("opened");
+    menu.classList.toggle("closed");
+    header.classList.toggle("actived");
 })
